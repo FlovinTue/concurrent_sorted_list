4aad03951803b4a61f867b077750d1f3032bfdeb
Flovin Tue Michaelsen <flovintue@outlook.com>
2019-08-30 12:36:34 +0200

    updated some external files

--- external/include/concurrent_object_pool.h
+++ external/include/concurrent_object_pool.h
@@ -1,3 +1,23 @@
+// Copyright(c) 2019 Flovin Michaelsen
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include <assert.h>
@@ -16,7 +36,7 @@ public:
 	inline Object* get_object();
 	inline void recycle_object(Object* object);
 
-	inline uint32_t avaliable() const;
+	inline std::size_t avaliable() const;
 
 	inline void unsafe_destroy();
 
@@ -65,7 +85,7 @@ inline void concurrent_object_pool<Object>::recycle_object(Object * object)
 	myUnusedObjects.push(object);
 }
 template<class Object>
-inline uint32_t concurrent_object_pool<Object>::avaliable() const
+inline std::size_t concurrent_object_pool<Object>::avaliable() const
 {
 	return static_cast<uint32_t>(myUnusedObjects.size());
 }
--- external/include/heap.h
+++ external/include/heap.h
@@ -14,49 +14,46 @@ public:
 	typedef uint64_t KeyType;
 
 	heap();
-	heap(const SizeType aInitCapacity);
+	heap(const SizeType initCapacity);
 
 	const SizeType size() const;
 
-	void push(const T &aIn, const KeyType aKey);
-	void push(T&& aIn, const KeyType aKey);
+	void push(const T &in, const KeyType key);
+	void push(T&& in, const KeyType key);
 
-	bool try_pop(T& aOut);
-	bool try_pop(T& aOutValue, KeyType& aOutKey);
+	bool try_pop(T& out);
+	bool try_pop(T& outValue, KeyType& outKey);
 
-	bool compare_try_pop(T& aOutValue, KeyType& aExpectedKey);
+	bool compare_try_pop(T& outValue, KeyType& expectedKey);
 
-	bool try_peek_top_key(KeyType& aOut);
+	bool try_peek_top_key(KeyType& out);
 
 	void clear();
 
 	void shrink_to_fit();
 
-	void reserve(const size_t aCapacity);
+	void reserve(const size_t capacity);
 
 private:
-	void trickle(SizeType aIndex);
-	void bubble(SizeType aIndex);
+	void trickle(SizeType index);
+	void bubble(SizeType index);
 
-	void pop_internal(T& aOutValue, KeyType& aOutKey);
+	void pop_internal(T& outValue, KeyType& outKey);
 
 	std::vector<std::pair<KeyType, T>> myStorage;
 
 	const Comparator myComparator;
-
-	std::atomic<SizeType> mySize;
 };
 template<class T, class Comparator>
 heap<T, Comparator>::heap()
 	: myComparator(Comparator())
-	, mySize(0)
 {
 }
 template<class T, class Comparator>
-inline heap<T, Comparator>::heap(const SizeType aInitCapacity)
+inline heap<T, Comparator>::heap(const SizeType initCapacity)
 	: heap()
 {
-	myStorage.reserve(aInitCapacity);
+	myStorage.reserve(initCapacity);
 }
 template<class T, class Comparator>
 const inline typename heap<T, Comparator>::SizeType heap<T, Comparator>::size() const
@@ -64,66 +61,57 @@ const inline typename heap<T, Comparator>::SizeType heap<T, Comparator>::size() 
 	return mySize.load(std::memory_order_relaxed);
 }
 template<class T, class Comparator>
-inline void heap<T, Comparator>::push(const T & aIn, const KeyType aKey)
+inline void heap<T, Comparator>::push(const T & in, const KeyType key)
 {
-	
-	myStorage.emplace_back(std::pair<KeyType, T>( aKey, aIn));
+	myStorage.emplace_back(std::pair<KeyType, T>( key, in));
 	bubble(myStorage.size() - 1);
-	mySize.fetch_add(1, std::memory_order_relaxed);
-	
 }
 template<class T, class Comparator>
-inline void heap<T, Comparator>::push(T && aIn, const KeyType aKey)
+inline void heap<T, Comparator>::push(T && in, const KeyType key)
 {
 	
-	myStorage.emplace_back(std::pair<KeyType, T>( aKey, std::move(aIn)));
+	myStorage.emplace_back(std::pair<KeyType, T>( key, std::move(in)));
 	bubble(myStorage.size() - 1);
-	mySize.fetch_add(1, std::memory_order_relaxed);
-	
 }
 template<class T, class Comparator>
-inline bool heap<T, Comparator>::try_pop(T & aOut)
+inline bool heap<T, Comparator>::try_pop(T & out)
 {
 	KeyType dummy(0);
-	return try_pop(aOut, dummy);
+	return try_pop(out, dummy);
 }
 template<class T, class Comparator>
-inline bool heap<T, Comparator>::try_pop(T & aOutValue, KeyType & aOutKey)
+inline bool heap<T, Comparator>::try_pop(T & outValue, KeyType & outKey)
 {
-	
-
 	if (!myStorage.size()) {
 		
 		return false;
 	}
 
-	pop_internal(aOutValue, aOutKey);
-
-	
+	pop_internal(outValue, outKey);
 
 	return true;
 }
 template<class T, class Comparator>
-inline void heap<T, Comparator>::trickle(SizeType aIndex)
+inline void heap<T, Comparator>::trickle(SizeType index)
 {
 	const SizeType size(myStorage.size());
 
-	if (aIndex == size - 1) {
+	if (index == size - 1) {
 		return;
 	}
 
-	SizeType index(aIndex);
-	while (index < size) {
-		const SizeType desiredLeftIndex(index * 2 + 1);
-		const SizeType desiredRightIndex(index * 2 + 2);
-		const SizeType leftIndex(desiredLeftIndex < size ? desiredLeftIndex : index);
-		const SizeType rightIndex(desiredRightIndex < size ? desiredRightIndex : index);
+	SizeType index_(index);
+	while (index_ < size) {
+		const SizeType desiredLeftIndex(index_ * 2 + 1);
+		const SizeType desiredRightIndex(index_ * 2 + 2);
+		const SizeType leftIndex(desiredLeftIndex < size ? desiredLeftIndex : index_);
+		const SizeType rightIndex(desiredRightIndex < size ? desiredRightIndex : index_);
 		
 		const KeyType& leftKey(myStorage[leftIndex].first);
 		const KeyType& rightKey(myStorage[rightIndex].first);
 
-		KeyType targetKey(myStorage[index].first);
-		SizeType targetIndex(index);
+		KeyType targetKey(myStorage[index_].first);
+		SizeType targetIndex(index_);
 
 		if (myComparator(leftKey, targetKey)) {
 			targetIndex = leftIndex;
@@ -134,103 +122,84 @@ inline void heap<T, Comparator>::trickle(SizeType aIndex)
 			targetKey = rightKey;
 		}
 
-		if (!(targetIndex ^ index)) {
+		if (!(targetIndex ^ index_)) {
 			break;
 		}
 
-		std::swap(myStorage[index], myStorage[targetIndex]);
+		std::swap(myStorage[index_], myStorage[targetIndex]);
 
-		index = targetIndex;
+		index_ = targetIndex;
 	}
 }
 template<class T, class Comparator>
-inline void heap<T, Comparator>::bubble(SizeType aIndex)
+inline void heap<T, Comparator>::bubble(SizeType index)
 {
-	SizeType index(aIndex);
-	SizeType parent((index - 1) / 2);
+	SizeType index_(index);
+	SizeType parent((index_ - 1) / 2);
 
 	while (true) {
-		if (!index)
+		if (!index_)
 			return;
 
-		if (!myComparator(myStorage[index].first, myStorage[parent].first))
+		if (!myComparator(myStorage[index_].first, myStorage[parent].first))
 			break;
 
-		std::swap(myStorage[index], myStorage[parent]);
-		index = parent;
-		parent = (index - 1) / 2;
+		std::swap(myStorage[index_], myStorage[parent]);
+		index_ = parent;
+		parent = (index_ - 1) / 2;
 	}
 }
 template<class T, class Comparator>
-inline void heap<T, Comparator>::pop_internal(T & aOutValue, KeyType & aOutKey)
+inline void heap<T, Comparator>::pop_internal(T & outValue, KeyType & outKey)
 {
-	aOutKey = myStorage[0].first;
-	aOutValue = std::move(myStorage[0].second);
+	outKey = myStorage[0].first;
+	outValue = std::move(myStorage[0].second);
 
 	std::swap(myStorage[0], myStorage[myStorage.size() - 1]);
 	myStorage.pop_back();
 
 	trickle(0);
-
-	mySize.fetch_sub(1, std::memory_order_relaxed);
 }
 template<class T, class Comparator>
 inline void heap<T, Comparator>::clear()
 {
-	
 	myStorage.clear();
-	mySize.store(0, std::memory_order_relaxed);
-	
 }
 template<class T, class Comparator>
 inline void heap<T, Comparator>::shrink_to_fit()
 {
-	
 	myStorage.shrink_to_fit();
-	
 }
 template<class T, class Comparator>
-inline void heap<T, Comparator>::reserve(const size_t aCapacity)
+inline void heap<T, Comparator>::reserve(const size_t capacity)
 {
-	
-	myStorage.reserve(aCapacity);
-	
+	myStorage.reserve(capacity);
 }
 template<class T, class Comparator>
-inline bool heap<T, Comparator>::compare_try_pop(T & aOutValue, KeyType & aExpectedKey)
+inline bool heap<T, Comparator>::compare_try_pop(T & outValue, KeyType & expectedKey)
 {
-	
 	if (!myStorage.size()) {
 		
 		return false;
 	}
-
-	if (myStorage[0].first != aExpectedKey) {
-		aExpectedKey = myStorage[0].first;
+	if (myStorage[0].first != expectedKey) {
+		expectedKey = myStorage[0].first;
 		
 		return false;
 	}
-
-	pop_internal(aOutValue, aExpectedKey);
-
-	
+	pop_internal(outValue, expectedKey);
 
 	return true;
 }
 
 template<class T, class Comparator>
-inline bool heap<T, Comparator>::try_peek_top_key(KeyType & aOut)
+inline bool heap<T, Comparator>::try_peek_top_key(KeyType & out)
 {
-	
-
 	if (!myStorage.size()) {
 		
 		return false;
 	}
-
-	aOut = myStorage[0].first;
-
-	
+	out = myStorage[0].first;
 
 	return true;
 }
@@ -238,8 +207,8 @@ inline bool heap<T, Comparator>::try_peek_top_key(KeyType & aOut)
 template <class T>
 struct TinyLess
 {
-	constexpr const bool operator()(const T& aA, const T& aB) const
+	constexpr const bool operator()(const T& a, const T& b) const
 	{
-		return aA < aB;
+		return a < b;
 	};
 };
